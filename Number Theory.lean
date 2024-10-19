aa27513a0f9ec86297ec0bf6f45075e235af7e1c
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2024-10-19 08:50:34 -0500

    Added theorem add_comm and improved documentation.
    Rewrote comments to more accurately reflect the way Lean proofs work, and moved them into the functions to improve readability.

--- Number Theory.lean
+++ Number Theory.lean
@@ -15,23 +15,29 @@ will solve the goal. For pedadagogical purposes, we will pretend that this is
 not the case, and will build up to this result from first principles.
 -/
 
-/-
-rfl proves goals of the form X = X.
-
-Example: 37 * x + q = 37 * x + q
-Proof: This is true by the reflexive property of equality
--/
 example (x q : Nat) : 37 * x + q = 37 * x + q := by
+  /-
+  rfl proves goals of the form X = X.
+
+  Example: 37 * x + q = 37 * x + q
+  Proof: This is true by the reflexive property of equality. QED
+  -/
   rfl
 
-/-
-If h is a proof of an equality X = Y, then rw [h] will change all Xs in the
-goal to Ys. It's the way to "substitute in".
-
-Example: If y = x + 7, then 2 * y = 2 * (x + 7)
-Proof: This is true by substiting y for x + 7 to get 2 * (x + 7) = 2 * (x + 7)
--/
 example (x y : Nat) (h : y = x + 7) : 2 * y = 2 * (x + 7) := by
+  /-
+  If h is a proof of an equality X = Y, then rw [h] will change all Xs in the
+  goal to Ys. It's the way to "substitute in".
+
+  Example: If y = x + 7, then 2 * y = 2 * (x + 7)
+  Proof: Substitute y for x + 7 and use the reflexivity of equality:
+
+  2 * y = 2 * (x + 7)
+
+  2 * (x + 7) = 2 * x + 7    [Substituting y = x + 7]
+
+  QED
+  -/
   rw [h]
 
 /-
@@ -47,90 +53,208 @@ theorem three_eq_succ_two : 3 = Nat.succ 2 := by
 theorem four_eq_succ_three : 4 = Nat.succ 3 := by
   rfl
 
-/-
-We can use rw to rewrite multiple things in one line. We can also reference
-any theorems that we have previously proven.
-
-Nat.succ n = n + 1, but we're going to pretend that addition isn't defined yet,
-and that the successor function is given to us by the Peano Axioms.
-
-Example: 2 = S(S(0))
-Proof: 2 = S(1) = S(S(0)).
--/
 example : 2 = Nat.succ (Nat.succ 0) := by
+  /-
+  We can use rw to rewrite multiple things in one line. We can also reference
+  any theorems that we have previously proven.
+
+  Nat.succ n = n + 1, but we're going to pretend that addition isn't defined yet,
+  and that the successor function is given to us by the Peano Axioms.
+
+  Example: 2 = S(S(0))
+  Proof: This follows from our definitions of 1 and 2:
+
+  2 = S(S(0))
+
+  S(1) = S(S(0))    [Definition of 2]
+
+  S(S(S(0))         [Definition of 1]
+
+  QED
+  -/
   rw [two_eq_succ_one, one_eq_succ_zero]
 
-/-
-If h is a proof of an equality X = Y, then rw [← h] will change all Ys in the
-goal to Xs. Type \l to get ← .
-
-Example: 2 = S(S(0))
-Proof: S(S(0)) = S(1) = 2. Note the difference compared to the previous example.
--/
 example : 2 = Nat.succ (Nat.succ 0) := by
-  rw [← one_eq_succ_zero, two_eq_succ_one]
+  /-
+  If h is a proof of an equality X = Y, then rw [← h] will change all Ys in the
+  goal to Xs. Type \l to get ← .
 
-/-
-repeat will repeatedly apply a tactic until it no longer succeeds.
+  Example: 2 = S(S(0))
+  Proof: This follows from our definitions of 1 and 2. Note the difference
+  compared to the previous example:
 
-Nat.add_zero replaces n + 0 with n. Note that we do not have the commutative
-property of addition; this function will not replace 0 + n with n.
+  2 = S(S(0))
 
-Example: a + (b + 0) + (c + 0) = a + b + c
-Proof: a + (b + 0) + (c + 0) = a + b + (c + 0) = a + b + c
--/
+  2 = S(1)    [Definition of 1]
+
+  2 = 2       [Definition of 2]
+
+  QED
+  -/
+  rw [← one_eq_succ_zero, ← two_eq_succ_one]
+
 example (a b c : Nat) : a + (b + 0) + (c + 0) = a + b + c := by
+  /-
+  repeat will repeatedly apply a tactic until it no longer succeeds.
+
+  Nat.add_zero replaces n + 0 with n. Note that we do not have the commutative
+  property of addition; this function will not replace 0 + n with n.
+
+  Example: a + (b + 0) + (c + 0) = a + b + c
+  Proof: Apply Peano's first axiom of addition, which states that for all n ∈ ℕ,
+  n + 0 = n, to both b + 0 and c + 0:
+
+  a + (b + 0) + (c + 0) = a + b + c
+
+  a + b + c = a + b + c    [Apply Peano's first axiom of addition twice.
+                           We rewrite b + 0 = b and c + 0 = c in a single step.]
+
+  QED
+  -/
   repeat rw [Nat.add_zero]
 
-/-
-Nat.succ_eq_add_one is the built-in method for this.
-
-Theorem: For all n ∈ ℕ, S(n) = n + 1.
-Proof: n + 1 = n + S(0) = S(n + 0) = S(n)
--/
 theorem succ_eq_add_one n : Nat.succ n = n + 1 := by
+  /-
+  Theorem: For all n ∈ ℕ, S(n) = n + 1.
+  Proof: We will need Peano's second axiom of addition, which states that for all
+  n ∈ ℕ, n + S(0) = S(n + 0):
+
+  S(n) = n + 1
+
+  S(n) = n + S(0)    [Definition of 1]
+
+  S(n) = S(n + 0)    [Peano's second axiom of addition]
+
+  S(n) = S(n)        [Peano's first axiom of addition]
+
+  QED
+  -/
   rw[one_eq_succ_zero, Nat.add_succ, Nat.add_zero]
 
-/-
-nth_rewrite i j will rewrite only the ith and jth occurrence of the expression
-to be rewritten. Indexing starts at 1.
-
-Example: 2 + 2 = 4
-Proof: 2 + 2 = 2 + S(1) = S(2 + 1) = S(2 + S(0)) = S(S(2 + 0)) = S(S(2)) = S(3) = 4
--/
 example : (2 : Nat) + 2 = 4 := by
+  /-
+  nth_rewrite i j will rewrite only the ith and jth occurrence of the expression
+  to be rewritten. Indexing starts at 1.
+
+  Example: 2 + 2 = 4
+  Proof: This follows directly from definitions and Peano's axioms of addition:
+
+  2 + 2 = 4
+
+  2 + S(1) = 4       [Definition of 2]
+
+  S(2 + 1) = 4       [Peano's second axiom of addition]
+
+  S(2 + S(0)) = 4    [Definition of 1]
+
+  S(S(2 + 0)) = 4    [Peano's second axiom of addition]
+
+  S(S(2)) = 4        [Peano's first axiom of addition]
+
+  S(3) = 4           [Definition of 3]
+
+  4 = 4              [Definition of 4]
+
+  QED
+  -/
   nth_rewrite 2 [two_eq_succ_one]
   rw [Nat.add_succ, one_eq_succ_zero, Nat.add_succ, Nat.add_zero, ← three_eq_succ_two, ← four_eq_succ_three]
 
-/-
-induction allows us to perform a proof by induction on n. This splits up into
-the base case "zero" corresponding to n = 0 and the inductive case "succ"
-corresponding to d → d + 1, in which we prove the inductive hypothesis ih.
-
-Theorem: For all n ∈ ℕ, 0 + n = n
-Proof: We perform induction on n.
-Base case: 0 + 0 = 0 is true by the identity property of addition.
-Inductive case: Assume that 0 + d = d. We must show that 0 + S(d) = S(d).
-This is true because 0 + S(d) = S(0 + d) = S(d).
--/
 theorem zero_add (n : Nat) : 0 + n = n := by
+  /-
+  induction allows us to perform a proof by induction on n. This splits up into
+  the base case "zero" corresponding to n = 0 and the inductive case "succ"
+  corresponding to d → d + 1, in which we prove the inductive hypothesis ih.
+
+  Theorem: For all n ∈ ℕ, 0 + n = n
+  Proof: We perform induction on n.
+
+  Base case:
+
+    0 + 0 = 0
+
+    0 = 0    [Peano's first axiom of addition]
+
+  Inductive case: Assume that 0 + d = d for some d ∈ ℕ.
+
+    0 + S(d) = S(d)
+
+    S(0 + d) = S(d)    [Peano's second axiom of addition]
+
+    S(d) = S(d)        [By the inductive hypothesis]
+
+  QED
+  -/
   induction n with
   | zero =>
     rw [Nat.add_zero]
   | succ d ih =>
     rw [Nat.add_succ, ih]
 
-/-
-Theorem: For all a, b ∈ ℕ, S(a) + b = S(a + b)
-Proof: We perform induction on b.
-Base case: S(a) + 0 = S(a) = S(a + 0)
-Inductive case: Assume that S(a) + d = S(a + d). We must show that
-S(a) + S(d) = S(a + S(d)). This is true because
-S(a) + S(d) = S(S(a) + d) = S(S(a + d)) = S(a + S(d)).
--/
 theorem succ_add (a b : Nat) : Nat.succ a + b = Nat.succ (a + b) := by
+  /-
+  Theorem: For all a, b ∈ ℕ, S(a) + b = S(a + b)
+  Proof: We perform induction on b.
+
+  Base case:
+
+    S(a) + 0 = S(a + 0)
+
+    S(a) = S(a)    [Apply Peano's first axiom of addition twice.]
+
+  Inductive case: Assume that S(a) + d = S(a + d) for some d ∈ ℕ.
+
+    S(a) + S(d) = S(a + S(d))
+
+    S(S(a) + d) = S(a + S(d))    [Peano's second axiom of addition]
+
+    S(S(a + d)) = S(a + S(d))    [The inductive hypothesis]
+
+    S(S(a + d)) = S(S(a + d))    [Peano's second axiom of addition]
+
+  QED
+  -/
   induction b with
   | zero =>
     repeat rw [Nat.add_zero]
   | succ d ih =>
     rw [Nat.add_succ, ih, Nat.add_succ]
+
+theorem add_comm (a b : Nat) : a + b = b + a := by
+  /-
+  Theorem: Addition of natural numbers is commutative. That is, for all
+  a, b ∈ ℕ, a + b = b + a.
+  Proof: We perform induction on a.
+
+  Base case:
+
+    0 + b = b + 0
+
+    0 + b = b    [Peano's first axiom of addition]
+
+    b = b        [Apply the `zero_add` theorem]
+
+  Inductive case: Assume that d + b = b + d for some d ∈ ℕ.
+
+    S(d) + b = b + S(d)
+
+    S(d) + b = S(b + d)    [Peano's second axiom of addition]
+
+    S(d + b) = S(b + d)    [Apply the `succ_add` theorem]
+
+    S(b + d) = S(b + d)    [The inductive hypothesis]
+
+  QED
+  -/
+  induction a with
+  | zero =>
+    rw [Nat.add_zero, Nat.zero_add]
+  | succ d ih =>
+    /-
+    rw [← Nat.succ_eq_add_one] is needed because Lean uses d + 1 rather than
+    S(d) for the inductive case.
+    -/
+    rw [← Nat.succ_eq_add_one]
+    rw [Nat.add_succ]
+    rw [Nat.succ_add]
+    rw [ih]
