cd56ad5ec4f4346b504304f83990257c6595f2f0
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2024-10-18 22:51:59 -0500

    Added succ_add and changed n++ to S(n).

--- Number Theory.lean
+++ Number Theory.lean
@@ -35,7 +35,7 @@ example (x y : Nat) (h : y = x + 7) : 2 * y = 2 * (x + 7) := by
   rw [h]
 
 /-
-Here we are defining 1 := 0++, 2 := 1++, 3 := 2++, and 4 := 3++, where n++
+Here we are defining 1 := S(0), 2 := S(1), 3 := S(2), and 4 := S(3), where S(n)
 denotes the successor of n.
 -/
 theorem one_eq_succ_zero : 1 = Nat.succ 0 := by
@@ -54,8 +54,8 @@ any theorems that we have previously proven.
 Nat.succ n = n + 1, but we're going to pretend that addition isn't defined yet,
 and that the successor function is given to us by the Peano Axioms.
 
-Example: 2 = (0++)++
-Proof: 2 = 1++ = (0++)++.
+Example: 2 = S(S(0))
+Proof: 2 = S(1) = S(S(0)).
 -/
 example : 2 = Nat.succ (Nat.succ 0) := by
   rw [two_eq_succ_one, one_eq_succ_zero]
@@ -64,8 +64,8 @@ example : 2 = Nat.succ (Nat.succ 0) := by
 If h is a proof of an equality X = Y, then rw [← h] will change all Ys in the
 goal to Xs. Type \l to get ← .
 
-Example: 2 = (0++)++
-Proof: (0++)++ = 1++ = 2. Note the difference compared to the previous example.
+Example: 2 = S(S(0))
+Proof: S(S(0)) = S(1) = 2. Note the difference compared to the previous example.
 -/
 example : 2 = Nat.succ (Nat.succ 0) := by
   rw [← one_eq_succ_zero, two_eq_succ_one]
@@ -85,8 +85,8 @@ example (a b c : Nat) : a + (b + 0) + (c + 0) = a + b + c := by
 /-
 Nat.succ_eq_add_one is the built-in method for this.
 
-Theorem: For all n ∈ ℕ, n++ = n + 1.
-Proof: n + 1 = n + 0++ = (n + 0)++ = n++
+Theorem: For all n ∈ ℕ, S(n) = n + 1.
+Proof: n + 1 = n + S(0) = S(n + 0) = S(n)
 -/
 theorem succ_eq_add_one n : Nat.succ n = n + 1 := by
   rw[one_eq_succ_zero, Nat.add_succ, Nat.add_zero]
@@ -96,7 +96,7 @@ nth_rewrite i j will rewrite only the ith and jth occurrence of the expression
 to be rewritten. Indexing starts at 1.
 
 Example: 2 + 2 = 4
-Proof: 2 + 2 = 2 + 1++ = (2 + 1)++ = (2 + 0++)++ = ((2 + 0)++)++ = (2++)++ = 3++ = 4
+Proof: 2 + 2 = 2 + S(1) = S(2 + 1) = S(2 + S(0)) = S(S(2 + 0)) = S(S(2)) = S(3) = 4
 -/
 example : (2 : Nat) + 2 = 4 := by
   nth_rewrite 2 [two_eq_succ_one]
@@ -104,13 +104,14 @@ example : (2 : Nat) + 2 = 4 := by
 
 /-
 induction allows us to perform a proof by induction on n. This splits up into
-the base case "zero" corresponding to n = 0 and the "succ" case d → d + 1, in
-which we prove the inductive hypothesis ih.
+the base case "zero" corresponding to n = 0 and the inductive case "succ"
+corresponding to d → d + 1, in which we prove the inductive hypothesis ih.
 
 Theorem: For all n ∈ ℕ, 0 + n = n
-Proof: 0 + 0 = 0 is true by the identity property of addition. Now, assume that
-0 + d = d. We must show that 0 + d++ = d++. This is true because 0 + d++ =
-(0 + d)++ = d++.
+Proof: We perform induction on n.
+Base case: 0 + 0 = 0 is true by the identity property of addition.
+Inductive case: Assume that 0 + d = d. We must show that 0 + S(d) = S(d).
+This is true because 0 + S(d) = S(0 + d) = S(d).
 -/
 theorem zero_add (n : Nat) : 0 + n = n := by
   induction n with
@@ -119,3 +120,17 @@ theorem zero_add (n : Nat) : 0 + n = n := by
   | succ d ih =>
     rw [Nat.add_succ, ih]
 
+/-
+Theorem: For all a, b ∈ ℕ, S(a) + b = S(a + b)
+Proof: We perform induction on b.
+Base case: S(a) + 0 = S(a) = S(a + 0)
+Inductive case: Assume that S(a) + d = S(a + d). We must show that
+S(a) + S(d) = S(a + S(d)). This is true because
+S(a) + S(d) = S(S(a) + d) = S(S(a + d)) = S(a + S(d)).
+-/
+theorem succ_add (a b : Nat) : Nat.succ a + b = Nat.succ (a + b) := by
+  induction b with
+  | zero =>
+    repeat rw [Nat.add_zero]
+  | succ d ih =>
+    rw [Nat.add_succ, ih, Nat.add_succ]
