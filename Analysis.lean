0dd3ca71dfe187930c14974a385682809e13a85a
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2024-11-08 22:14:17 -0600

    Added relevant theorems from number_theory.lean

--- Analysis.lean
+++ Analysis.lean
@@ -17,3 +17,33 @@ Definition of Addition:
   (1) Nat.add_zero (n : Nat) : 0 + n = n
   (2) Nat.add_succ (n m : Nat) : n + m.succ = (n + m).succ
 -/
+
+theorem zero_add (n : Nat) : 0 + n = n := by
+  induction n with
+  | zero =>
+    rw [Nat.add_zero]
+  | succ d ih =>
+    rw [Nat.add_succ, ih]
+
+theorem succ_add (n m : Nat) : n.succ + m = (n + m).succ := by
+  induction m with
+  | zero =>
+    repeat rw [Nat.add_zero]
+  | succ d ih =>
+    rw [Nat.add_succ, ih, Nat.add_succ]
+
+theorem add_comm (n m : Nat) : n + m = m + n := by
+  induction n with
+  | zero =>
+    rw [Nat.add_zero, Nat.zero_add]
+  | succ d ih =>
+    rw [← Nat.succ_eq_add_one, Nat.add_succ, Nat.succ_add, ih]
+
+theorem add_assoc (a b c : Nat) : (a + b) + c = a + (b + c) := by
+  induction a with
+  | zero =>
+    repeat rw [Nat.zero_add]
+  | succ d ih =>
+    rw [← Nat.succ_eq_add_one]
+    repeat rw [Nat.succ_add]
+    rw [ih]
