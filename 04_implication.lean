0580cbccf4e393fd964e3fc070102f6bf6f4874d
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2024-11-15 13:53:32 -0600

    Added example for exact

--- 04_implication.lean
+++ 04_implication.lean
@@ -1,1 +1,11 @@
+import «Number Theory».Basic
 
+/-
+  We now advance to proving implications, i.e., if-then statements. The main
+  tool we use to prove statements of this type is the `exact`. Compared to
+  `rfl` and `rw`, `exact` is very strict, and requires, as the name implies,
+  that the input match the goal exactly.
+-/
+
+example (x y : Nat) (h1 : x + y = 37) : x + y = 37 := by
+  exact h1
