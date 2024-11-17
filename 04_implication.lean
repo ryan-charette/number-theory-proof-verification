0a2ec9abc9ebd8213a853ac2f7207c3c74d99104
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2024-11-16 23:27:32 -0600

    Commented example

--- 04_implication.lean
+++ 04_implication.lean
@@ -8,4 +8,12 @@ import «Number Theory».Basic
 -/
 
 example (x y : Nat) (h1 : x + y = 37) : x + y = 37 := by
+  /-
+  Theorem: If x + y = 37, then x + y = 37.
+  Proof: This is tautological; assume the hypothesis. Then
+
+  x + y = 37
+
+  QED
+  -/
   exact h1
